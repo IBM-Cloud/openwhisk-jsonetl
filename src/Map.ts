3f71fbb6ab5bec885ae6228a21c4a57deed74924
Van Staub <van_staub@us.ibm.com>
2018-08-15 09:08:51 -0400

    optional path and deletion

--- src/Map.ts
+++ src/Map.ts
@@ -1,13 +1,14 @@
 import Jsonata, { JsonataParams } from './Jsonata';
 
 export interface MapParams {
-  _path: string;
   _input: string;
   _output: string;
+  _path?: string;
+  _delete?: boolean;
 }
 
 export default function main(params: any): any {
-  let { _path: path, _input: input, _output: output } = params;
+  let { _path: path, _input: input, _output: output, _delete: del = true } = params;
 
   const inKeys: string[] = input.split(',');
   const outKeys: string[] = output.split(',');
@@ -16,6 +17,7 @@ export default function main(params: any): any {
     throw Error(`Map input and output keys do not match`);
   }
 
+  // Jsonata will handle an undefined path by returning the original object
   const obj = Jsonata({...params as any, ...{_jsonata: path, _toObj: false} as JsonataParams});
 
   if (!obj) {
@@ -28,7 +30,10 @@ export default function main(params: any): any {
 
   inKeys.forEach((key, i) => {
     obj[outKeys[i]] = obj[key];
-    delete obj[key];
+
+    if (del) {
+      delete obj[key];
+    }
   });
 
   return obj;
