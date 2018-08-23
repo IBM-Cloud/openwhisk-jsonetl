bc24601e2556ca4cfeacc34ce868aca82c6fde7a
Van Staub <van_staub@us.ibm.com>
2018-08-23 15:26:26 -0400

    0.0.6

--- dist/Debug.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function main(params) {
-    console.log(JSON.stringify(params));
-    return {
-        _data: params
-    };
-}
-exports.default = main;
--- src/Debug.ts
+++
@@ -1,8 +0,0 @@
-import Data from './Data';
-
-export default function main(params: any): Data {
-  console.log(JSON.stringify(params));
-  return {
-    _data: params
-  }
-}
